bdad00d9d7d2dab985a8ece64ad474215a83fd80
badamziom <badamziom@o2.pl>
2015-03-10 21:36:27 +0100

    Join between category and product

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,7 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @categories = Category.all
   end
 
   def create
@@ -63,7 +64,7 @@ class BooksController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def book_params
-    params.require(:book).permit(:name, :image, :description, :image, :status)
+    params.require(:book).permit(:name, :image, :description, :image, :status, :category_ids => [])
   end
 
 end
